751382f472fd612d70a6392f677c798f0f167212
javier <jfernandes113@gmail.com>
2023-07-14 20:15:05 +0200

    auth and users

---
+++ Backend/controllers/auth/signout.js
@@ -0,0 +1,18 @@
+const { hash, serialize } = require('simple-stateless-auth-library')
+const errors = require('../../misc/errors')
+const { selectUser } = require('../../models/auth')
+//falta crear modelo para auth
+
+module.exports = (db) => async (req, res, next) => {
+    const { email, password } = req.body
+
+    const response = await selectUser(await db)(email, hash.compare(password))
+
+    if(!response.ok) return next(errors[response.error_code || 500])
+
+    serialize(res, response.content)
+
+    res.status(200).json({
+        success: true,
+    })
+}
